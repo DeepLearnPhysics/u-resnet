485854868e321da98c4b12c768863e8c83fe81f4
drinkingkazu <kazuhiro@nevis.columbia.edu>
2017-10-26 23:05:25 -0400

    minor change

--- resnet_module.py
+++ resnet_module.py
@@ -3,7 +3,7 @@ import tensorflow.python.platform
 import tensorflow as tf
 import uresnet_layers as L
 
-def double_resnet(input_tensor, dim_factor=2, filter_factor=2, input_filters=64, num_classes=4, kernel=(3,3), step=1):
+def double_resnet(input_tensor, dim_factor=2, num_filters=64, kernel=(3,3), step=1):
 
     # need to batch normalize 
 
@@ -13,25 +13,25 @@ def double_resnet(input_tensor, dim_factor=2, filter_factor=2, input_filters=64,
 
     # 1st conv layer
     net = L.conv2d(input_tensor=net, name='conv1_1_%d' %step, kernel=kernel, stride=(dim_factor,dim_factor), 
-                   num_filter=filter_factor*input_filters, activation_fn=tf.nn.relu)
+                   num_filter=num_filters, activation_fn=tf.nn.relu)
     # 2nd conv layer
     net = L.conv2d(input_tensor=net, name='conv1_2_%d' %step, kernel=kernel, stride=(1,1), 
-                   num_filter=filter_factor*input_filters, activation_fn=tf.nn.relu)
+                   num_filter=num_filters, activation_fn=tf.nn.relu)
 
     # decrease spatial dimensions and increase channel number of mod1 output                                                                         
     mod1 = net
    # mod1 = L.avg_pool(input_tensor=mod1, name='decreaseSpatialD', kernel=(1,1), stride=(1,1))
     mod1 = L.conv2d(input_tensor=mod1, name='adjustDimensions_%d' %step,kernel=(1,1), stride=(1,1), 
-                    num_filter=filter_factor*input_filters, activation_fn=tf.nn.relu)
+                    num_filter=num_filters, activation_fn=tf.nn.relu)
 
     # Module 2
 
     # 3rd conv layer                                                                                                                                
     net = L.conv2d(input_tensor=net, name='conv2_1_%d' %step, kernel=kernel, stride=(1,1), 
-                   num_filter=filter_factor*input_filters, activation_fn=tf.nn.relu)
+                   num_filter=num_filters, activation_fn=tf.nn.relu)
     # 4th conv layer                                                                                                                                
     net = L.conv2d(input_tensor=net, name='conv2_2_%d' %step, kernel=kernel, stride=(1,1),
-                   num_filter=filter_factor*input_filters, activation_fn=tf.nn.relu)
+                   num_filter=num_filters, activation_fn=tf.nn.relu)
 
     # add the modified mod1 output to current tensor
     net += mod1
--- uresnet.py
+++ uresnet.py
@@ -4,111 +4,78 @@ import tensorflow as tf
 import uresnet_layers as L
 from resnet_module import double_resnet
 
-def build(input_tensor, num_class=4):
+def build(input_tensor, num_class=4, base_num_filters=16, debug=False):
 
     net = input_tensor
+    if debug: print(net.shape, 'input shape')
 
     # assume zero padding in each layer (set as default in uresnet_layers.py)
 
     # downsampling path
+    num_filters = base_num_filters
+
+    # feature maps saved for later upsampling layer
+    feature_map = {}
 
     # initial 7x7 convolution
-    net = L.conv2d(input_tensor=net, name='conv7x7_begin', kernel=(7,7), stride=(1,1), num_filter=64, activation_fn=tf.nn.relu)
-    downsampling0 = net # save for concatenation with upsampling0 later
-    # max pool
-    net = L.max_pool(input_tensor=net, name="max_pool", kernel=(3,3), stride=(2,2))
+    net = L.conv2d(input_tensor=net, name='conv7x7_begin', kernel=(7,7), stride=(1,1), num_filter=num_filters, activation_fn=tf.nn.relu)
+    # save for concatenation with upsampling0 later
+    feature_map[(net.get_shape()[-2].value,net.get_shape()[-3].value)] = net
+    if debug: print(net.shape, "after conv7x7_begin")
 
-    # 2x resnet: input is 256 x 256 x 64
-    net = double_resnet(input_tensor=net, filter_factor=1, dim_factor=1, input_filters=64, num_classes=num_class, step=0)
-    downsampling1 = net
-
-    # 2x resnet: input is 256 x 256 x 64
-    net = double_resnet(input_tensor=net, filter_factor=2, dim_factor=2, input_filters=64, num_classes=num_class, step=1)
-    downsampling2 = net
-
-    # 2x resnet: input is 128 x 128 x 128
-    net = double_resnet(input_tensor=net, filter_factor=2, dim_factor=2, input_filters=128, num_classes=num_class, step=2)
-    downsampling3 = net
-
-    # 2x resnet: input is 64 x 64 x 256
-    net = double_resnet(input_tensor=net, filter_factor=2, dim_factor=2, input_filters=256, num_classes=num_class, step=3)
-    downsampling4 = net
-
-    # 2x resnet: input is 32 x 32 x 512
-    print(net.shape, "before step 4")
-    net = double_resnet(input_tensor=net, filter_factor=2, dim_factor=2, input_filters=512, num_classes=num_class, step=4)
-    print(net.shape, "after step 4")
+    # downsampling path
+    if debug: print(net.shape, "before downsampling")
+    step = 0
+    for _ in xrange(5):
+        step += 1
+        num_filters *= 2
+        net = double_resnet(input_tensor=net, dim_factor=2, num_filters=num_filters,  step=step)
+        fmap_key = (net.get_shape()[-2].value,net.get_shape()[-3].value)
+        feature_map[fmap_key] = net
+        if debug: print(net.shape, "after downsampling step", step)
 
     # upsampling path
+    if debug: print(net.shape, "before upsampling")
+    for _ in xrange(4):
+        step += 1
+        num_filters /= 2
+        # deconvolution
+        net = L.deconv2d(input_tensor=net, name='deconv_%d' % step, kernel=(3,3), stride=(2,2), output_num_filter=num_filters)
+        if debug: print(net.shape, "after upsampling deconv", step)
 
-    # deconvolution 0
-    net = L.deconv2d(input_tensor=net, name='deconv_0', kernel=(3,3), stride=(2,2), output_num_filter=512) 
-    print(net.shape, "afterdeconv0")
+        # concatenate feature map
+        fmap_key = (net.get_shape()[-2].value,net.get_shape()[-3].value)
+        upsampling   = net
+        downsampling = feature_map[fmap_key]
+        net = tf.concat([downsampling, upsampling], axis=3)
+        if debug: print(net.shape, "after upsampling concat", step)    
 
-    # concatenate downsampling4 and upsampling4
-    upsampling4 = net # could go with "net" but just for readability, define new variable
-    net = tf.concat([downsampling4, upsampling4], axis=3)
+        # 2x resnet indexed 5: input is 32 x 32 x (512+512)
+        net = double_resnet(input_tensor=net, dim_factor=1, num_filters=num_filters,  step=step)
+        if debug: print(net.shape, "after upsampling", step)
     
-    # 2x resnet indexed 5: input is 32 x 32 x (512+512)
-    net = double_resnet(input_tensor=net, filter_factor=0.5, dim_factor=1, input_filters=1024, num_classes=num_class, step=5)
-    print(net.shape, "after step 5, should be 32 32 512")
-    
-    # deconvolution 1
-    net = L.deconv2d(input_tensor=net, name='deconv_1', kernel=(4,4), stride=(2,2), output_num_filter=256)
-    print(net.shape, "afterdeconv1, should be 64 64 256")
-
-    # concatenate downsampling3 and upsampling3    
-    upsampling3 = net
-    net = tf.concat([downsampling3, upsampling3], axis=3)
-
-    # 2x resnet indexed 6: input is 64 x 64 x (256+256)
-    net = double_resnet(input_tensor=net, filter_factor=0.5, dim_factor=1, input_filters=512, num_classes=num_class, step=6)
-    print(net.shape, "after step 6, should be 64 64 256")
-    
-    # deconvolution 2
-    net = L.deconv2d(input_tensor=net, name='deconv_2', kernel=(4,4), stride=(2,2), output_num_filter=128)
-    print(net.shape, "afterdeconv2, should be 128 128 128")
-
-    # concatenate downsampling2 and upsampling2
-    upsampling2 = net
-    net = tf.concat([downsampling2, upsampling2], axis=3)
-
-    # 2x resnet indexed 7: input is 128 x 128 x (128+128)
-    net = double_resnet(input_tensor=net, filter_factor=0.5, dim_factor=1, input_filters=256, num_classes=num_class, step=7)
-    print(net.shape, "after step 7, should be 128 128 128")
-
-    # deconvolution 3
-    net = L.deconv2d(input_tensor=net, name='deconv_3', kernel=(4,4), stride=(2,2), output_num_filter=64)
-    print(net.shape, "afterdeconv3, should be 256 256 64")
-
-    # concatenate downsampling1 and upsampling1
-    upsampling1 = net
-    net = tf.concat([downsampling1, upsampling1], axis=3)
-
-    # 2x resnet indexed 8: input is 256 x 256 x (64+64), note kernel size in this module is (5,5) not (3,3)
-    net = double_resnet(input_tensor=net, filter_factor=0.5, dim_factor=1, kernel=(5,5), input_filters=128, num_classes=num_class, step=8)
-    print(net.shape, "after step 8, should be 256 256 64")
-
     # deconvolution 4
-    net = L.deconv2d(input_tensor=net, name='deconv_4', kernel=(4,4), stride=(2,2), output_num_filter=64)
-    print(net.shape, "afterdeconv3, should be 512 512 64")
+    net = L.deconv2d(input_tensor=net, name='deconv%d' % (step+1), kernel=(3,3), stride=(2,2), output_num_filter=base_num_filters)
+    if debug: print(net.shape, "after upsampling deconv", (step+1))
 
     # concatenate downsampling0 and upsampling0
-    upsampling0 = net
-    net = tf.concat([downsampling0, upsampling0], axis=3)
+    upsampling = net
+    fmap_key = (net.get_shape()[-2].value,net.get_shape()[-3].value)
+    downsampling = feature_map[fmap_key]
+    net = tf.concat([downsampling, upsampling], axis=3)
 
     # first of two  7x7 conv at the end
-    net = L.conv2d(input_tensor=net, name='conv7x7_end1', kernel=(7,7), stride=(1,1), num_filter=64, activation_fn=tf.nn.relu)
-    print(net.shape, "should be 512 512 64")
+    net = L.conv2d(input_tensor=net, name='conv7x7_end1', kernel=(7,7), stride=(1,1), num_filter=base_num_filters, activation_fn=tf.nn.relu)
+    if debug: print(net.shape, "after conv7x7_end1")
 
     # second of two 7x7 conv at the end
     net = L.conv2d(input_tensor=net, name='conv7x7_end2', kernel=(7,7), stride=(1,1), num_filter=num_class, activation_fn=tf.nn.relu)
-    print(net.shape, "should be 512 512 num_class")
+    if debug: print(net.shape, "after conv7x7_end2")
 
     return net
 
 # script unit test
 if __name__ == '__main__':
     x = tf.placeholder(tf.float32, [50,512,512,1])
-    net = build(x)
+    net = build(input_tensor=x,debug=True)
     
